96191af1135af16ce639e6b480898a2e56f48fce
Demur Rumed <gunkmute@gmail.com>
2012-06-21 14:04:37 +0000

    Makeshift binpack

--- synch.c
+++ synch.c
@@ -4,6 +4,12 @@ typedef struct{
 	char*n;
 	png_bytep s;
 }spr;
+typedef struct{
+	unsigned short x,y,w,h;
+	unsigned a;
+}rect;
+rect*fr;
+int frs=1;
 int sprcmp(void*a,void*b)
 {
 	return ((spr*)b)->h-((spr*)a)->h;
@@ -13,27 +19,88 @@ int main(int argc,char**argv){
 	fputs("const unsigned char ",f);
 	int Fmt=-1;
 	spr S[argc-1];
+	unsigned Swid=1,Shei=1;
 	for(int i=1;i<argc;i++){
 		char nm[strlen(argv[i])+9];
 		sprintf(nm,"spr/%s.png",argv[i]);
 		readpng(nm);
-		S[i-1].x=0;
-		S[i-1].y=0;
-		S[i-1].w=wi4;
-		S[i-1].h=hei;
-		S[i-1].s=road;
-		S[i-1].n=argv[i];
-		int fmt=csz;
-		if(Fmt==-1)Fmt=csz;
-		else if(Fmt!=csz)abort_("%s conflicting formats %d %d\n",argv[i],Fmt,csz);
+		if(Fmt!=csz){
+			if(Fmt==-1)Fmt=csz;
+			else abort_("%s conflicting formats %d %d\n",argv[i],Fmt,csz);
+		}
+		if(wi4>Swid)Swid=wi4;
+		if(hei>Shei)Shei=hei;
+		spr*s=S+i-1;
+		s->x=0;
+		s->y=0;
+		s->w=wi4;
+		s->h=hei;
+		s->s=road;
+		s->n=argv[i];
 	}
 	qsort(S,argc-1,sizeof(spr),(__compar_fn_t)sprcmp);
-	int xacc=0;
+	fr=malloc(sizeof(rect));
+	fr->x=0;
+	fr->y=0;
+	fr->w=Swid=np2(Swid);
+	fr->h=Shei=np2(Shei);
+	fr->a=fr->w*fr->h;
 	for(int i=0;i<argc-1;i++){
-		S[i].x=xacc;
-		xacc+=S[i].w;
+		spr*s=S+i;
+		newjj:;
+		int f=-1,mna=Swid*Shei;
+		for(int j=0;j<frs;j++){
+			if(fr[j].a<mna&&s->w<fr[j].w&&s->h<fr[j].h&&s->w*s->h<=fr[j].a){
+				mna=fr[j].a;
+				f=j;
+			}
+		}
+		if(f==-1){
+			if(Swid<Shei){
+				for(int j=0;j<frs;j++)
+					if(fr[j].x+fr[j].w==Swid){
+						fr[j].w+=Swid;
+						fr[j].a=fr[j].w*fr[j].h;
+					}
+				Swid<<=1;
+			}
+			else{
+				for(int j=0;j<frs;j++)
+					if(fr[j].y+fr[f].h==Shei){
+						fr[j].h+=Shei;
+						fr[j].a=fr[j].w*fr[j].h;
+					}
+				Shei<<=1;
+			}
+			goto newjj;
+		}
+		s->x=fr[f].x;
+		s->y=fr[f].y;
+		if(s->w==fr[f].w||s->h==fr[f].h){
+			if(s->w==fr[f].w&&s->h==fr[f].h){
+				memmove(fr+f,fr+f+1,(frs-f-1)*sizeof(rect));
+				frs--;
+			}else if(s->w==fr[f].w){
+				fr[f].y+=s->h;
+				fr[f].h-=s->h;
+				fr[f].a=fr[f].w*fr[f].h;
+			}else{
+				fr[f].x+=s->w;
+				fr[f].w-=s->w;
+				fr[f].a=fr[f].w*fr[f].h;
+			}
+		}else{
+			fr=realloc(fr,(++frs)*sizeof(rect));
+			fr[frs-1].x=fr[f].x;
+			fr[frs-1].y=fr[f].y+s->h;
+			fr[frs-1].w=s->w;
+			fr[frs-1].h=fr[f].h-s->h;
+			fr[frs-1].a=fr[frs-1].w*fr[frs-1].h;
+			fr[f].x+=s->w;
+			fr[f].w-=s->w;
+			fr[f].a=fr[f].w*fr[f].h;
+		}
 	}
-	int Swid=np2(xacc),Shei=np2(S[0].h);
 	fprintf(f,"S[%d]=\"",Swid*Shei*csz);
 	unsigned char Sdata[Swid*Shei*csz];
 	memset(Sdata,0,sizeof(Sdata));
